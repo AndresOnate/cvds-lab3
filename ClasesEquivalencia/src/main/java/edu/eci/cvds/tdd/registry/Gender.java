b24de71318e8da4247abfae1947e7358c51a2db2
ANDRES CAMILO OÑATE QUIMBAYO <63562181+AndresOnate@users.noreply.github.com>
2023-02-25 22:54:50 -0500

    Delete ClasesEquivalencia directory

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Gender.java
+++
@@ -1,5 +0,0 @@
-package edu.eci.cvds.tdd.registry;
-
-public enum Gender {
-    MALE, FEMALE, UNIDENTIFIED;   
-}
--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/RegisterResult.java
+++
@@ -1,5 +0,0 @@
-package edu.eci.cvds.tdd.registry;
-
-public enum RegisterResult {
-    DEAD, UNDERAGE, INVALID_AGE, VALID, DUPLICATED
-}
